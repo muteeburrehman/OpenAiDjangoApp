9c0e4456c25a1fdb4d7629965e3436d81518edf0
muteeburrehman <muteeb285@gmail.com>
2025-03-17 00:30:41 +0500

    Token and user serializer and view setup

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 # internals
 from api.models import CodeExplainer
 from api.utils import send_code_to_api
@@ -22,8 +24,32 @@ class CodeExplainSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model: User
+        model = User
         fields = ["id","username", "email", "password"]
         extra_kwargs = {
             "password":{"write_only":True}
         }
+
+    def create(self, validated_data):
+         password = validated_data.pop("password")
+         user = User.objects.create(**validated_data)
+         #  to save the password as hashed and crypted
+         user.set_password(password)
+         user.save()
+         # for token based auth
+         Token.objects.create(user=user)
+         return user
+    
+class TokenSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(style={"input_type":"password"}, trim_whitespace=False)
+
+    def validate(self, attrs):
+        username = attrs.get("username")
+        password = attrs.get("password")
+        user = authenticate(request=self.context.get("request"),username=username,password=password) 
+        if not user:
+            msg = "Credentials are not provided correctly..."
+            raise serializers.ValidationError(msg, code="authentication")
+        attrs["user"] = user
+        return attrs   
